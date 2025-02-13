63f638df0bf23d5b9113be9d976d7ae63ab7c68a
Raghu K <raghuk.vit@gmail.com>
2025-02-13 06:30:21 +0530

    RoboSHop Project

--- argocd.sh
+++ argocd.sh
@@ -3,17 +3,29 @@ env=$2
 
 export PATH=/github-runner/.local/bin:/github-runner/bin:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin
 
+ARGOCD_LOGIN() {
+    argocd_ip=$(kubectl get svc -n argocd argocd-server -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+    argocd_password=$(kubectl get secret argocd-initial-admin-secret -n argocd -o=jsonpath='{.data.password}' | base64 --decode)
+    argocd login ${argocd_ip} --insecure --username admin --password ${argocd_password}
+}
+
 if [ -z "$app_name" -o -z "$env" ]; then
   echo Input AppName or env is missing
   exit 1
 fi
 
 # If not logged in we will login
+if [ ! -e ~/.config/argocd/config ]; then
+  ARGOCD_LOGIN
+fi
+
+current_ip=$(cat ~/.config/argocd/config  | grep ^current-context | awk '{print $NF}')
+nc -w 3 -z $current_ip 443 &>/dev/null
+if [ $? -ne 0 ]; then ARGOCD_LOGIN ; fi
+
 argocd account list &>/dev/null
 if [ $? -ne 0 ]; then
-  argocd_ip=$(kubectl get svc -n argocd argocd-server -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
-  argocd_password=$(kubectl get secret argocd-initial-admin-secret -n argocd -o=jsonpath='{.data.password}' | base64 --decode)
-  argocd login ${argocd_ip} --insecure --username admin --password ${argocd_password}
+  ARGOCD_LOGIN
 fi
 
 argocd app create ${app_name} --repo https://github.com/raghudevopsb82/roboshop-helm.git --dest-namespace default --dest-server https://kubernetes.default.svc --values env-${env}/${app_name}.yaml  --path .
