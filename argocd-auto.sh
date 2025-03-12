c02a08ace7f070db31626ff427cf353f61c3e558
Raghu K <raghuk.vit@gmail.com>
2025-03-12 06:54:31 +0530

    RoboSHop Project

--- argocd-auto.sh
+++ argocd-auto.sh
@@ -5,7 +5,7 @@ argocd_ip=$(kubectl get svc -n argocd argocd-server -o jsonpath='{.status.loadBa
 argocd_password=$(kubectl get secret argocd-initial-admin-secret -n argocd -o=jsonpath='{.data.password}' | base64 --decode)
 argocd login argocd-dev.azdevopsb82.online:443 --insecure --username admin --password ${argocd_password} --grpc-web
 
-for app_name in cart ; do
+for app_name in cart catalogue user shipping payment frontend; do
 
 appImage=$(gh search commits --repo raghudevopsb82/roboshop-$app_name "RoboSHop" | head -1 | awk '{print $2}')
 argocd app create --upsert roboshop-${app_name} --repo https://github.com/raghudevopsb82/roboshop-helm.git --dest-namespace default --dest-server https://kubernetes.default.svc --values env-dev/${app_name}.yaml  --path . --helm-set appImage=$appImage --grpc-web
